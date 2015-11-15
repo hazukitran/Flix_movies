7d890aa189e678a4599bca6375f039e194233a08
Lexi Tran <hazukitran@gmail.com>
2015-11-15 12:06:23 +0700

    Add form and footer partials

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,7 +13,6 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
-    movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
     @movie.update(movie_params)
     redirect_to @movie
   end
@@ -23,10 +22,14 @@ class MoviesController < ApplicationController
   end
 
   def create
-    movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
     @movie = Movie.new(movie_params)
     @movie.save
     redirect_to @movie
   end
 
+private
+  def movie_params
+    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+  end
+  
 end
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,7 +1,7 @@
 module MoviesHelper
   def format_total_gross(movie)
     if movie.flop?
-      "Flop!"
+      content_tag(:strong, "Flop!")
     else
       number_to_currency(movie.total_gross)
     end
