79cfc5b7a0d4d6a7992a9e573f8fd96d96e08156
Lexi Tran <hazukitran@gmail.com>
2015-11-15 16:40:58 +0700

    Add custom queries

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,22 @@
 class Movie < ActiveRecord::Base
+  
   def flop?
     total_gross.blank? || total_gross < 50000000 
   end
+
+  def self.released
+    where("released_on < ?", Time.now).order(released_on: :desc)
+  end
+
+  # def self.hit_movies
+  #   where("total_gross > ?", 300000000).order(total_gross: :desc)
+  # end
+
+  # def self.flop_movies
+  #   where("total_gross < ?", 50000000).order(:total_gross)
+  # end
+
+  # def self.recently_added_movies
+  #   order(created_at: :desc)
+  # end
 end
