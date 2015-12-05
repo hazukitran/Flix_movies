79b7f76498c2da5c845dce8090d246b2337c727d
Lexi Tran <hazukitran@gmail.com>
2015-12-05 16:32:42 +0700

    Sign In

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user, notice: "Thanks for signing up!"
     else
       render :new
@@ -42,6 +43,6 @@ class UsersController < ApplicationController
 private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :name, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ActiveRecord::Base
   					format: /\A\S+@\S+\z/,
   					uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 10, allow_blank: true }
+  validates :username, presence: true,
+  						format: /\A[A-Z0-9]+\z/i,
+  						uniqueness: { case_sensitive: false }
+  
+  def gravatar_id
+  	Digest::MD5::hexdigest(email.downcase)
+  end
+
+  def self.authenticate(email_or_username, password)
+    user = User.find_by(email: email_or_username) || User.find_by(username: email_or_username)
+    user && user.authenticate(password)
+  end
 end
