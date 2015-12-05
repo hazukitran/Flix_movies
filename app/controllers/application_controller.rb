26307d220d93c5d55889031a33aebd6e06526ab2
Lexi Tran <hazukitran@gmail.com>
2015-12-05 23:58:30 +0700

    Add admin users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,4 +24,16 @@ private
   end
 
   helper_method :current_user?
+
+  def require_admin
+    unless current_user_admin?
+      redirect_to root_path, alert: "Unauthorized access!"
+    end
+  end
+
+  def current_user_admin?
+    current_user && current_user.admin?
+  end
+
+  helper_method :current_user_admin?
 end
