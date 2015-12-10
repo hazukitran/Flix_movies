bcdcf47fb3a280583b04abda8d330edfa7fc4dbd
Lexi Tran <hazukitran@gmail.com>
2015-12-10 18:00:34 +0700

    Custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   get "signup" => "users#new"
   resources :users
 
+  # get "movies/filter/flops" => "movies#index", scope: "flops"
+  # get "movies/filter/hits" => "movies#index", scope: "hits"
+  get "movies/filter/:scope" => "movies#index", as: "filtered_movies"
   root "movies#index"
   resources :movies do
     resources :reviews
